39eb52eaf6c5c75119ff6a0dde892b2dd21cc90c
Joshua Introne <jintrone@mit.edu>
2012-11-10 01:10:54 -0500

    Fixing bugginess with HIT description

--- src/main/java/edu/mit/cci/amtprojects/solver/SolverHitCreator.java
+++ src/main/java/edu/mit/cci/amtprojects/solver/SolverHitCreator.java
@@ -61,7 +61,7 @@ public class SolverHitCreator implements HitCreator {
         if (status.getPhase() == SolverProcessMonitor.Phase.INIT) {
             DefaultEnabledHitProperties props = new DefaultEnabledHitProperties();
             props.setTitle("Rank a set of answers to a question about climate change "+groupText);
-            props.setDescription("Rank a set of answers to the question: " + model.getQuestionText() + "  Bonus of up to $." + String.format("%f.2", model.getMaxRankingBonus()));
+            props.setDescription("Rank a set of answers to the question: " + model.getQuestionText() + "  Bonus of up to $." + String.format("%2f", model.getMaxRankingBonus()));
             props.setKeywords("climate,experiment,rank,bonus");
             props.setMaxAssignments("" + model.getNumberOfRankers());
             props.setRewardAmount("" + model.getBaseReward());
@@ -75,7 +75,7 @@ public class SolverHitCreator implements HitCreator {
         } else if (status.getPhase() == SolverProcessMonitor.Phase.GENERATE) {
             DefaultEnabledHitProperties props = new DefaultEnabledHitProperties();
             props.setTitle("Create or improve an answer to a question about climate change "+groupText);
-            props.setDescription("Choose to create a new answer or improve existing answers to the question: " + model.getQuestionText() + "  Bonus of up to $." + String.format("%f.2", Math.max(model.getMaxGeneratingBonus(), model.getMaxCombiningBonus())));
+            props.setDescription("Choose to create a new answer or improve existing answers to the question: " + model.getQuestionText() + "  Bonus of up to $." + String.format("%2f", Math.max(model.getMaxGeneratingBonus(), model.getMaxCombiningBonus())));
             props.setKeywords("climate,experiment,bonus");
             props.setMaxAssignments("" + model.getNumberOfGenerators());
             props.setRewardAmount("" + model.getBaseReward());
@@ -89,7 +89,7 @@ public class SolverHitCreator implements HitCreator {
         } else if (status.getPhase() == SolverProcessMonitor.Phase.RANK) {
             DefaultEnabledHitProperties props = new DefaultEnabledHitProperties();
             props.setTitle("Rank a set of answers to a question about climate change ["+model.getGroupName()+"]" );
-            props.setDescription("Rank a set of answers to the question: " + model.getQuestionText() + "  Bonus of up to $." + String.format("%f.2", model.getMaxRankingBonus()));
+            props.setDescription("Rank a set of answers to the question: " + model.getQuestionText() + "  Bonus of up to $." + String.format("%2f", model.getMaxRankingBonus()));
             props.setKeywords("climate,experiment,rank,bonus");
             props.setMaxAssignments("" + model.getNumberOfRankers());
             props.setRewardAmount("" + model.getBaseReward());
